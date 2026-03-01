f0b0c51563c0b2163daf5c9852ed0e9e91448c21
Giahensum <gianam2k4@gmail.com>
2026-03-01 16:47:46 +0700

    transition map normal to map boss

---
+++ Assets/Script/TransitionGateMap/Portal.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [Tooltip("Gõ đúng tên Scene Boss vào đây")]
+    public string nextSceneName = "Boss1Scene";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Nếu cái chạm vào cổng có Tag là Player
+        if (collision.CompareTag("Player"))
+        {
+            // Gọi Canvas khép màn hình và chuyển map
+            SceneTransitionManager.Instance.TeleportToMap(nextSceneName);
+        }
+    }
+}
